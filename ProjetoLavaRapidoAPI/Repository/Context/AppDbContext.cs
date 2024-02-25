f379f08b95519c8b889a990925e7c80069f3e129
monica88lima <flima.monica@gmail.com>
2024-02-25 13:50:57 -0300

    database connection

---
+++ ProjetoLavaRapidoAPI/Repository/Context/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Context
+{    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Veiculo> Veiculos { get; set; }
+    }
+}
