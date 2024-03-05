53f26a2514d25439cba6fc2f6a87c6e6caf6520c
ROBERTO PACHECO CLARO <roberto.claro@viavarejo.com.br>
2024-03-05 10:30:06 -0300

    criando projeto

---
+++ lava-rapido/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Status from './Pages/Status/Status';
+import Timeline from './Pages/Timeline/Timeline';
+import Menu from './Components/Menu/Menu';
+import DefaultPage from './Pages/DefaultPage/DefaultPage';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>REACT</h1>
+    </div>
+  );
+}
+
+export default App;
