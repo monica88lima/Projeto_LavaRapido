4ef391961dc0544f906fbc981a0bf17ebaf01c1a
monica88lima <flima.monica@gmail.com>
2024-03-06 22:01:55 -0300

    ajuste cors

--- ProjetoLavaRapidoAPI/ProjetoLavaRapidoAPI/Program.cs
+++ ProjetoLavaRapidoAPI/ProjetoLavaRapidoAPI/Program.cs
@@ -12,6 +12,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder =>
+        {
+            builder
+                .WithOrigins("http://localhost:3000")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddControllers().AddJsonOptions(options =>
                                                  options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
